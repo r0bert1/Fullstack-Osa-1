8f60a3afc36d945935fd364633a160207455e860
Robert <robert.pakkanen@helsinki.fi>
2019-01-21 12:40:39 +0200

    Osa 1. Kolmas tehtävä

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const Header = (props) => {
 const Content = (props) => {
     return (
         <div>
-            <Part part1={props.part1} exercises1={props.exercises1} />
-            <Part part2={props.part2} exercises2={props.exercises2} />
-            <Part part3={props.part3} exercises3={props.exercises3} />
+            <Part part1={props.part1.name} exercises1={props.part1.exercises} />
+            <Part part2={props.part2.name} exercises2={props.part2.exercises} />
+            <Part part3={props.part3.name} exercises3={props.part3.exercises} />
         </div>
     )
 }
@@ -32,28 +32,33 @@ const Part = (props) => {
 const Total = (props) => {
     return (
         <div>  
-            <p> yhteensä {props.exercises1 + props.exercises2 + props.exercises3} tehtävää </p> 
+            <p> yhteensä {props.part1.exercises + props.part2.exercises + props.part3.exercises} tehtävää </p> 
         </div>
     )
 }
 
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
+  const part1 = {
+      name: 'Reactin perusteet',
+      exercises: 10
+  }
+  const part2 = {
+    name: 'Tiedonvälitys propseilla',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'Komponenttien tila',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3}/>
     </div>
   )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
